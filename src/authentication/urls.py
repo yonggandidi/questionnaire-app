64dc7f1cf4c93aecee04f84e43d2f77eb33edc9c
Jessie Jia <jessiejia@Jessies-MacBook-Pro.local>
2020-06-15 16:09:45 -0700

    user retrieve

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RegistrationAPIView, LoginAPIView
+from .views import RegistrationAPIView, LoginAPIView, UserRetrieveUpdateAPIView
 
 app_name = 'authentication'
 urlpatterns = [
     path('users/', RegistrationAPIView.as_view()),
-    path('users/login/', LoginAPIView.as_view())
+    path('users/login/', LoginAPIView.as_view()),
+    path('user', UserRetrieveUpdateAPIView.as_view()),
 ]
